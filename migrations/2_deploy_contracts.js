27bfc5cd345529f8183c11ee46978e09605a8651
Sharu Jiang <jsrshark110@gmail.com>
2018-12-16 17:50:17 -0800

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,3 @@
-/*
-var DotToken = artifacts.require("DotToken");
-
-module.exports = function(deployer) {
-  deployer.deploy(DotToken, 0x147D434e85e5D80552C8d5610C335e17ecAdCAa1);
-};
-*/
-
 var VoteProposal = artifacts.require("VoteProposal");
 
 module.exports = function(deployer) {
